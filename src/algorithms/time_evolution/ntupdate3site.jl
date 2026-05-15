0c98d63948a2a57607d704be0a1f31176674fed7
Yue Zhengyuan <yuezy1997@icloud.com>
2026-05-15 11:02:16 +0800

    Improve bond tensor for middle tensors in the cluster

--- src/algorithms/time_evolution/ntupdate3site.jl
+++ src/algorithms/time_evolution/ntupdate3site.jl
@@ -14,109 +14,82 @@ function _ntu_iter(
         state::InfiniteState, gate::Vector{T}, wts::SUWeight,
         sites::Vector{CartesianIndex{2}}, alg::NeighbourUpdate
     ) where {T <: AbstractTensorMap}
-    truncs = _get_cluster_trunc(alg.opt_alg.trunc, sites)
-    state, wts = copy(state), deepcopy(wts)
+    state2, wts2 = copy(state), deepcopy(wts)
 
-    Ms, invperms = _get_cluster_permute(state, sites)
+    # apply gate MPO
+    Ms, invperms = _get_cluster_permute(state2, sites)
     flips = [isdual(space(M, 1)) for M in Iterators.drop(Ms, 1)]
     _flip_virtuals!(Ms, flips) # flip virtual arrows in `Ms` to ←
-
-    # apply gate MPO without truncation
     _apply_gatempo!(Ms, gate)
-    _flip_virtuals!(Ms, flips) # restore virtual arrows in `Ms`
+
+    # put un-truncated tensors in `state2`
+    # arrow direction is temporarily changed
     for (M, s, invperm) in zip(Ms, sites, invperms)
-        state[s] = permute(M, invperm)
+        state2[s] = permute(M, invperm)
     end
+    
+    # Vidal gauge projectors
+    truncs = _get_cluster_trunc(alg.opt_alg.trunc, sites)
+    Pas, Pbs = _get_allprojs(Ms, truncs)
+    _flip_virtuals!(Pas, Pbs, flips)
 
-    # truncate each bond sequentially along the path
-    info = (; fid = 1.0)
-    nbond = length(sites) - 1
-    for (i, bondsites) in enumerate(zip(sites, Iterators.drop(sites, 1)))
-        trunc = truncs[i]
-        alg′ = (@set alg.opt_alg.trunc = trunc)
-        stype1 = (i == 1) ? :first : :middle
-        stype2 = (i == nbond) ? :last : :middle
-        state, wts, info′ = _bond_truncate(state, wts, bondsites, (stype1, stype2), alg′)
+    # bond-wise truncation
+    # TODO: reduce code duplication
+    fid = 1.0
+    for (i, (siteA, siteB)) in enumerate(zip(sites, Iterators.drop(sites, 1)))
+        # rotate to standard x direction `A ← B`
+        bond, rev = _nn_bondrev(siteA, siteB)
+        dir = first(bond)
+        state2 = _bond_rotation(state2, dir, rev; inv = false)
+        wts2 = _bond_rotation(wts2, dir, rev; inv = false)
+
+        # rotated bond tensors
+        ucell = size(state2)[1:2]
+        siteA′ = _bond_rotation(siteA, dir, rev, ucell)
+        row, col = siteA′[1], siteA′[2]
+        A, B = state2[row, col], state2[row, col + 1]
+
+        # apply projectors on current bond
+        # (also restoring arrow direction)
+        Pa, Pb = Pas[i], Pbs[i]
+        A = apply_projector(A, Pa)
+        B = apply_projector(Pb, B)
+
+        # factor out bond tensor
+        a, X = bond_tensor_first(A; positive = true)
+        b, Y = bond_tensor_last(B; positive = true)
+
+        # create bond environment
+        benv = bondenv_ntu(row, col, X, Y, state2, alg.bondenv_alg)
+        @debug "cond(benv) before gauge fix: $(LinearAlgebra.cond(benv))"
+        if alg.fixgauge
+            Z = positive_approx(benv)
+            Z, a, b, (Linv, Rinv) = fixgauge_benv(Z, a, b)
+            X = _fixgauge_benvX(X, Rinv)
+            Y = _fixgauge_benvY(Y, Linv)
+            benv = Z' * Z
+            @debug "cond(L) = $(LinearAlgebra.cond(Linv)); cond(R): $(LinearAlgebra.cond(Rinv))"
+            @debug "cond(benv) after gauge fix: $(LinearAlgebra.cond(benv))"
+        end
+
+        # truncation
+        opt_alg = alg.opt_alg
+        @reset opt_alg.trunc = truncs[i]
+        a, s, b, info = bond_truncate(a, b, benv, opt_alg)
         # record the worst fidelity
-        (info′.fid < info.fid) && (info = info′)
-    end
-    return state, wts, info
-end
+        (info.fid < fid) && (fid = info.fid)
 
-"""
-Truncate a nearest neighbor bond between `site1` and `site2`
-after rotating the bond to standard x direction `A ← B`.
+        A = undo_bond_tensor_first(a, X)
+        B = undo_bond_tensor_last(b, Y)
+        state2[row, col] = normalize!(A, Inf)
+        state2[row, col + 1] = normalize!(B, Inf)
+        wts2[1, row, col] = normalize!(s, Inf)
 
-`bondtype` takes values in (1, 2, 3), meaning that the current bond is
-(the first, a middle, the last) bond in the updated cluster.
-"""
-function _bond_truncate(
-        state::InfiniteState, wts::SUWeight,
-        (site1, site2)::NTuple{2, CartesianIndex{2}},
-        (stype1, stype2)::NTuple{2, Symbol},
-        alg::NeighbourUpdate; gate::Union{NNGate, Nothing} = nothing
-    )
-    # rotate bond to standard x direction `A ← B`
-    ucell = size(state)[1:2]
-    bond, rev = _nn_bondrev(site1, site2)
-    dir = first(bond)
-    state2 = _bond_rotation(state, dir, rev; inv = false)
-    wts2 = _bond_rotation(wts, dir, rev; inv = false)
-
-    # rotated bond tensors
-    siteA = _bond_rotation(site1, dir, rev, ucell)
-    row, col = siteA[1], siteA[2]
-    A, B = state2[row, col], state2[row, col + 1]
-
-    # create bond environment
-    qrtrunc = trunctol(; rtol = 1.0e-12)
-    a, X = if stype1 == :first
-        bond_tensor_first(A; trunc = qrtrunc)
-    else
-        @assert stype1 == :middle
-        bond_tensor_midnext(A; trunc = qrtrunc)
-    end
-    b, Y = if stype2 == :last
-        bond_tensor_last(B; trunc = qrtrunc)
-    else
-        @assert stype2 == :middle
-        bond_tensor_midprev(B; trunc = qrtrunc)
-    end
-    benv = bondenv_ntu(row, col, X, Y, state2, alg.bondenv_alg)
-    @debug "cond(benv) before gauge fix: $(LinearAlgebra.cond(benv))"
-    if alg.fixgauge
-        Z = positive_approx(benv)
-        Z, a, b, (Linv, Rinv) = fixgauge_benv(Z, a, b)
-        X = _fixgauge_benvX(X, Rinv)
-        Y = _fixgauge_benvY(Y, Linv)
-        benv = Z' * Z
-        @debug "cond(L) = $(LinearAlgebra.cond(Linv)); cond(R): $(LinearAlgebra.cond(Rinv))"
-        @debug "cond(benv) after gauge fix: $(LinearAlgebra.cond(benv))"
+        # rotate back tensors and bond weight
+        # TODO: only restore orientation after truncating all bonds
+        state2 = _bond_rotation(state2, dir, rev; inv = true)
+        wts2 = _bond_rotation(wts2, dir, rev; inv = true)
     end
 
-    # (optional) apply the NN gate without truncation
-    if !(gate === nothing)
-        a, s, b, = _apply_gate(a, b, gate, truncerror(; atol = 1.0e-15))
-    end
-    a, s, b, info = bond_truncate(a, b, benv, alg.opt_alg)
-
-    A = if stype1 == :first
-        undo_bond_tensor_first(a, X)
-    else
-        undo_bond_tensor_midnext(a, X)
-    end
-    B = if stype2 == :last
-        undo_bond_tensor_last(b, Y)
-    else
-        undo_bond_tensor_midprev(b, Y)
-    end
-
-    state2[row, col] = normalize!(A, Inf)
-    state2[row, col + 1] = normalize!(B, Inf)
-    wts2[1, row, col] = normalize!(s, Inf)
-
-    # rotate back tensors and bond weight
-    state2 = _bond_rotation(state2, dir, rev; inv = true)
-    wts2 = _bond_rotation(wts2, dir, rev; inv = true)
-    return state2, wts2, info
+    return state2, wts2, (; fid = 1.0)
 end
